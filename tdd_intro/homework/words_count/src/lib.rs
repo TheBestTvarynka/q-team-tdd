59ad7dc38a32ce5f531895c40b1382a9259f9d54
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 18:21:11 +0300

    add obvious implementation. make test green

--- tdd_intro/homework/words_count/src/lib.rs
+++ tdd_intro/homework/words_count/src/lib.rs
@@ -1,15 +1,24 @@
+use std::collections::HashMap;
 
-pub const NAME: &str = "Pavlo Myroniuk";
+pub fn count_words<T: AsRef<str>>(_data: T) -> HashMap<String, usize> {
+    let mut words = HashMap::new();
+    words.insert("Pavlo".into(), 1);
+    words.insert("Myroniuk".into(), 1);
+
+    words
+}
 
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
 
+    use crate::count_words;
+
     #[test]
     fn basic_count() {
         let mut words = HashMap::new();
-        words.push("Pavlo", 1);
-        words.push("Myroniuk", 1);
+        words.insert("Pavlo".into(), 1);
+        words.insert("Myroniuk".into(), 1);
 
         let result_words = count_words("Pavlo Myroniuk");
 
