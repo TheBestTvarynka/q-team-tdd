fc97e621e366f21666855c133d405a8acae59b5a
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 18:29:09 +0300

    add more red tests for words_count

--- tdd_intro/homework/words_count/src/lib.rs
+++ tdd_intro/homework/words_count/src/lib.rs
@@ -24,4 +24,37 @@ mod tests {
 
         assert_eq!(words, result_words);
     }
+
+    #[test]
+    fn empty_input() {
+        let words = HashMap::new();
+
+        let result_words = count_words("");
+
+        assert_eq!(words, result_words);
+    }
+
+    #[test]
+    fn long_input() {
+        let mut words = HashMap::new();
+        words.insert("acceptation".into(), 1);
+        words.insert("of".into(), 3);
+        words.insert("word".into(), 1);
+        words.insert("world".into(), 1);
+        words.insert("kind".into(), 1);
+        words.insert("was".into(), 1);
+        words.insert("he".into(), 1);
+        words.insert("a".into(), 2);
+        words.insert("in".into(), 1);
+        words.insert("zoologist".into(), 1);
+        words.insert("cuvier".into(), 1);
+        words.insert("new".into(), 1);
+        words.insert("then".into(), 1);
+        words.insert("the".into(), 3);
+        words.insert("entire".into(), 1);
+
+        let result_words = count_words("was he then a zoologist in the entire acceptation of the word a kind of cuvier of the new world");
+
+        assert_eq!(words, result_words);
+    }
 }
