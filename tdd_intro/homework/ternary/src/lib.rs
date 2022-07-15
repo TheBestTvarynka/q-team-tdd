d239277d751f3510fbf91d7d40645eee950226d6
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 22:46:17 +0300

    ternary: add tests for invalid input (red)

--- tdd_intro/homework/ternary/src/lib.rs
+++ tdd_intro/homework/ternary/src/lib.rs
@@ -31,4 +31,12 @@ mod tests {
         assert_eq!(27, to_decimal("1000"));
         assert_eq!(35, to_decimal("001022"));
     }
+
+    #[test]
+    fn invalid_input() {
+        assert_eq!(0, to_decimal("132512"));
+        assert_eq!(0, to_decimal("1205"));
+        assert_eq!(0, to_decimal("6000"));
+        assert_eq!(0, to_decimal("3201022"));
+    }
 }
