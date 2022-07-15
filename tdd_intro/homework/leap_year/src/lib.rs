bfb9da1e6ee4e7a14fa7bb9df282a63ae41ac6c8
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 22:10:38 +0300

    leap_year: add tests for 100th and 400th years (red so far)

--- tdd_intro/homework/leap_year/src/lib.rs
+++ tdd_intro/homework/leap_year/src/lib.rs
@@ -16,6 +16,19 @@ mod tests {
     }
 
     #[test]
+    fn not_leap_100th() {
+        assert!(!is_leap(1700));
+        assert!(!is_leap(1800));
+        assert!(!is_leap(1900));
+    }
+
+    #[test]
+    fn leap_400() {
+        assert!(is_leap(1600));
+        assert!(is_leap(2000));
+    }
+
+    #[test]
     fn basic_leap() {
         assert!(is_leap(2020));
         assert!(is_leap(1996));
