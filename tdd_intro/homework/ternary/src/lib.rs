fb830de26350fd563e40cd2bfc161247660dd18f
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 22:23:29 +0300

    ternary: write obvious implementation (tests are green)

--- tdd_intro/homework/ternary/src/lib.rs
+++ tdd_intro/homework/ternary/src/lib.rs
@@ -1,6 +1,12 @@
 
+pub fn to_decimal<T: AsRef<str>>(_data: T) -> u32 {
+    302
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::to_decimal;
+
     #[test]
     fn basic_conversion() {
         assert_eq!(302, to_decimal("102012"));
