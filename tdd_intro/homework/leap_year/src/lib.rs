34bf52d551e361b95efbcb5001c43dd8362fc10d
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 20:18:39 +0300

    leap_year: add obvious implementation

--- tdd_intro/homework/leap_year/src/lib.rs
+++ tdd_intro/homework/leap_year/src/lib.rs
@@ -1,6 +1,12 @@
 
+pub fn is_leap(_year: u32) -> bool {
+    false
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::is_leap;
+
     #[test]
     fn basic_not_leap() {
         assert!(!is_leap(2021));
