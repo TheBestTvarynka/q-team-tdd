5f8079b2c26a5a9fc3307327290a846ff163c66b
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 22:32:03 +0300

    ternary: improve tests (red)

--- tdd_intro/homework/ternary/src/lib.rs
+++ tdd_intro/homework/ternary/src/lib.rs
@@ -8,7 +8,10 @@ mod tests {
     use crate::to_decimal;
 
     #[test]
-    fn basic_conversion() {
+    fn valid_input() {
         assert_eq!(302, to_decimal("102012"));
+        assert_eq!(46, to_decimal("1201"));
+        assert_eq!(27, to_decimal("1000"));
+        assert_eq!(35, to_decimal("001022"));
     }
 }
