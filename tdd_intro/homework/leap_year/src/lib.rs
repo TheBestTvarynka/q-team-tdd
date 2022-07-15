d8f0de75b3a94878f4979fe505e9110f6d738642
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 22:04:35 +0300

    leap_year: imrpove existing and add new tests

--- tdd_intro/homework/leap_year/src/lib.rs
+++ tdd_intro/homework/leap_year/src/lib.rs
@@ -10,5 +10,14 @@ mod tests {
     #[test]
     fn basic_not_leap() {
         assert!(!is_leap(2021));
+        assert!(!is_leap(1997));
+        assert!(!is_leap(1998));
+        assert!(!is_leap(1233));
+    }
+
+    #[test]
+    fn basic_leap() {
+        assert!(is_leap(2020));
+        assert!(is_leap(1996));
     }
 }
