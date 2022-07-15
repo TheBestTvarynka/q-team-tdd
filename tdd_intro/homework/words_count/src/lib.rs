caefb9277b40b36fb4904bc1484ab2abd45582e3
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-15 18:16:01 +0300

    add basic red test for words count

--- tdd_intro/homework/words_count/src/lib.rs
+++ tdd_intro/homework/words_count/src/lib.rs
@@ -3,8 +3,16 @@ pub const NAME: &str = "Pavlo Myroniuk";
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     #[test]
-    fn test() {
-        assert_eq!(2 + 3, 5);
+    fn basic_count() {
+        let mut words = HashMap::new();
+        words.push("Pavlo", 1);
+        words.push("Myroniuk", 1);
+
+        let result_words = count_words("Pavlo Myroniuk");
+
+        assert_eq!(words, result_words);
     }
 }
