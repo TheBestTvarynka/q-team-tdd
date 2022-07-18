e42c83036a33f9492337bbb03a7062a39ef1d026
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-18 13:30:13 +0300

    bank-ocr: add more tests (red)

--- tdd_intro/homework/bank-ocr/src/lib.rs
+++ tdd_intro/homework/bank-ocr/src/lib.rs
@@ -1,8 +1,8 @@
 use std::io;
 
 
-pub fn parse_account_number<T: AsRef<str>>(_data: T) -> io::Result<[u8; 9]> {
-    Ok([1, 2, 3, 4, 5, 6, 7, 8, 9])
+pub fn parse_account_number<T: AsRef<str>>(_data: T) -> io::Result<Vec<[u8; 9]>> {
+    Ok(vec![[1, 2, 3, 4, 5, 6, 7, 8, 9]])
 }
 
 #[cfg(test)]
@@ -11,6 +11,23 @@ mod tests {
 
     #[test]
     fn simple() {
-        assert_eq!([1, 2, 3, 4, 5, 6, 7, 8, 9], parse_account_number(include_str!("../assets/simple.txt")).unwrap());
+        assert_eq!(vec![[1, 2, 3, 4, 5, 6, 7, 8, 9]], parse_account_number(include_str!("../assets/simple.txt")).unwrap());
+    }
+
+    #[test]
+    fn empty() {
+        assert_eq!(Vec::<[u8; 9]>::new(), parse_account_number("").unwrap());
+    }
+
+    #[test]
+    fn multiple_numbers() {
+        assert_eq!(
+            vec![
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [9, 2, 3, 7, 5, 6, 7, 3, 9],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            parse_account_number(include_str!("../assets/multiple_inputs.txt")).unwrap()
+        );
     }
 }
