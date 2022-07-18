0ca419b38522d552a707c29e3e37f7e60a51ea37
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-18 12:23:22 +0300

    bank-ocr: init crate; write simple test (red)

---
+++ tdd_intro/homework/bank-ocr/src/lib.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+
+pub fn parse_account_number<T: AsRef<str>>(_data: T) -> io::Result<[u8; 9]> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_account_number;
+
+    #[test]
+    fn simple() {
+        assert_eq!([1, 2, 3, 4, 5, 6, 7, 8, 9], parse_account_number(include_str!("../assets/simple.txt")).unwrap());
+    }
+}
