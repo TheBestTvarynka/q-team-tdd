452b2e413c81304e85474076a9f932282a764a3b
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-18 17:00:04 +0300

    bank-ocr: add tests with invalid input

--- tdd_intro/homework/bank-ocr/src/lib.rs
+++ tdd_intro/homework/bank-ocr/src/lib.rs
@@ -15,12 +15,13 @@ pub fn parse_account_number<T: AsRef<str>>(data: T) -> io::Result<Vec<[u8; 9]>> 
     let mut lines = data.lines();
 
     while let Some(number_top) = lines.next() {
+        println!("numbers_top: {}", number_top);
         let number_middle = lines
             .next()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount"))?;
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount: no second line"))?;
         let number_bottom = lines
             .next()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount"))?;
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount: no third line"))?;
 
         if number_top.len() != LINE_LEN
             || number_middle.len() != LINE_LEN
@@ -99,5 +100,23 @@ mod tests {
             ],
             parse_account_number(include_str!("../assets/multiple_inputs.txt")).unwrap()
         );
+    }
+
+    #[test]
+    fn long_input() {
+        assert_eq!(486, parse_account_number(include_str!("../assets/long_input.txt")).unwrap().len());
+    }
+
+    #[test]
+    fn invalid_lines_amount() {
+        assert!(parse_account_number(include_str!("../assets/one_line.txt")).is_err());
+        assert!(parse_account_number(include_str!("../assets/two_lines.txt")).is_err());
+    }
+
+    #[test]
+    fn invalid_number() {
+        assert!(parse_account_number(include_str!("../assets/ten_digits.txt")).is_err());
+        assert!(parse_account_number(include_str!("../assets/eight_digits.txt")).is_err());
+        assert!(parse_account_number(include_str!("../assets/invalid_digit.txt")).is_err());
     }
 }
