1f29abb4f77259028e6a3b001f0d5b91d45f8555
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-07-18 16:38:57 +0300

    bank-ocr: refactor parsing algorithm (all tests are green)

--- tdd_intro/homework/bank-ocr/src/lib.rs
+++ tdd_intro/homework/bank-ocr/src/lib.rs
@@ -1,5 +1,11 @@
+mod digit_literals;
+
 use std::io;
 
+use digit_literals::{EIGHT, FIVE, FOUR, NINE, ONE, SEVEN, SIX, THREE, TWO, ZERO};
+
+// 3 /*digit width*/ * 9 /*digits amount*/
+const LINE_LEN: usize = 3 * 9;
 
 pub fn parse_account_number<T: AsRef<str>>(data: T) -> io::Result<Vec<[u8; 9]>> {
     let data = data.as_ref();
@@ -8,46 +14,55 @@ pub fn parse_account_number<T: AsRef<str>>(data: T) -> io::Result<Vec<[u8; 9]>> 
 
     let mut lines = data.lines();
 
-    loop {
-        let number_top = if let Some(line) = lines.next() {
-            line
-        } else {
-            break;
-        };
+    while let Some(number_top) = lines.next() {
+        let number_middle = lines
+            .next()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount"))?;
+        let number_bottom = lines
+            .next()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount"))?;
 
-        let number_middle = lines.next().ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount"))?;
-        let number_bottom = lines.next().ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid lines amount"))?;
-
-        println!("Invalid lines len: {} {} {}", number_top.len(), number_middle.len(), number_bottom.len());
-
-        if number_top.len() != 27 || number_middle.len() != 27 || number_bottom.len() != 27 {
-            return Err(io::Error::new(io::ErrorKind::Other, format!("Invalid lines len: {} {} {}", number_top.len(), number_middle.len(), number_bottom.len())));
+        if number_top.len() != LINE_LEN
+            || number_middle.len() != LINE_LEN
+            || number_bottom.len() != LINE_LEN
+        {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!(
+                    "Invalid lines len: {} {} {}",
+                    number_top.len(),
+                    number_middle.len(),
+                    number_bottom.len()
+                ),
+            ));
         }
 
         let mut account_number = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        for i in 0..9 {
-            let index = i * 3;
-            let digit_top = &number_top[index..index + 3];
-            let digit_middle = &number_middle[index..index + 3];
-            let digit_bottom = &number_bottom[index..index + 3];
+        for (index, item) in account_number.iter_mut().enumerate() {
+            let index = index * 3;
 
-            println!("`{}`", digit_top);
-            println!("`{}`", digit_middle);
-            println!("`{}`", digit_bottom);
-
-            match (digit_top, digit_middle, digit_bottom) {
-                (" _ ", "| |", "|_|") => account_number[i] = 0,
-                ("   ", "  |", "  |") => account_number[i] = 1,
-                (" _ ", " _|", "|_ ") => account_number[i] = 2,
-                (" _ ", " _|", " _|") => account_number[i] = 3,
-                ("   ", "|_|", "  |") => account_number[i] = 4,
-                (" _ ", "|_ ", " _|") => account_number[i] = 5,
-                (" _ ", "|_ ", "|_|") => account_number[i] = 6,
-                (" _ ", "  |", "  |") => account_number[i] = 7,
-                (" _ ", "|_|", "|_|") => account_number[i] = 8,
-                (" _ ", "|_|", " _|") => account_number[i] = 9,
-                _ => return Err(io::Error::new(io::ErrorKind::Other, format!("Invalid number:\n{}\n{}\n{}", digit_top, digit_middle, digit_bottom))),
+            match [
+                &number_top[index..index + 3],
+                &number_middle[index..index + 3],
+                &number_bottom[index..index + 3],
+            ] {
+                ZERO => *item = 0,
+                ONE => *item = 1,
+                TWO => *item = 2,
+                THREE => *item = 3,
+                FOUR => *item = 4,
+                FIVE => *item = 5,
+                SIX => *item = 6,
+                SEVEN => *item = 7,
+                EIGHT => *item = 8,
+                NINE => *item = 9,
+                data => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("Invalid number: {:?}", data),
+                    ))
+                }
             };
         }
 
@@ -63,7 +78,10 @@ mod tests {
 
     #[test]
     fn simple() {
-        assert_eq!(vec![[1, 2, 3, 4, 5, 6, 7, 8, 9]], parse_account_number(include_str!("../assets/simple.txt")).unwrap());
+        assert_eq!(
+            vec![[1, 2, 3, 4, 5, 6, 7, 8, 9]],
+            parse_account_number(include_str!("../assets/simple.txt")).unwrap()
+        );
     }
 
     #[test]
